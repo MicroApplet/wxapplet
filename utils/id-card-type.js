b17676a230dca69747fe537261cb4917ab84df21
Asial Jim <asialjim@qq.com>
2025-10-16 03:34:03 +0000

    添加available字段以动态控制证件类型可用性

--- utils/id-card-type.js
+++ utils/id-card-type.js
@@ -3,72 +3,86 @@ const IdCardType = {
   ResidentIdentityCard: {
     code: '01',
     enName: 'ResidentIdentityCard',
-    cnName: '居民身份证'
+    cnName: '居民身份证',
+    available: true
   },
   HouseholdRegister: {
     code: '02',
     enName: 'HouseholdRegister',
-    cnName: '户口簿'
+    cnName: '户口簿',
+    available: false
   },
   DriverLicense: {
     code: '03',
     enName: 'DriverLicense',
-    cnName: '驾驶证'
+    cnName: '驾驶证',
+    available: false
   },
   SocialSecurityCard: {
     code: '04',
     enName: 'SocialSecurityCard',
-    cnName: '社会保障卡'
+    cnName: '社会保障卡',
+    available: false
   },
   MilitaryID: {
     code: '05',
     enName: 'MilitaryID',
-    cnName: '军官证'
+    cnName: '军官证',
+    available: false
   },
   PoliceOfficerID: {
     code: '06',
     enName: 'PoliceOfficerID',
-    cnName: '警官证'
+    cnName: '警官证',
+    available: false
   },
   OfficerID: {
     code: '07',
     enName: 'OfficerID',
-    cnName: '公务员证'
+    cnName: '公务员证',
+    available: false
   },
   HongKongMacaoPass: {
     code: '08',
     enName: 'HongKongMacaoPass',
-    cnName: '港澳通行证'
+    cnName: '港澳通行证',
+    available: false
   },
   TaiwanPass: {
     code: '09',
     enName: 'TaiwanPass',
-    cnName: '台湾通行证'
+    cnName: '台湾通行证',
+    available: false
   },
   BorderPass: {
     code: '10',
     enName: 'BorderPass',
-    cnName: '边境通行证'
+    cnName: '边境通行证',
+    available: false
   },
   SeamanBook: {
     code: '11',
     enName: 'SeamanBook',
-    cnName: '海员证'
+    cnName: '海员证',
+    available: false
   },
   LawyerLicense: {
     code: '12',
     enName: 'LawyerLicense',
-    cnName: '律师执业证'
+    cnName: '律师执业证',
+    available: false
   },
   ForResCard: {
     code: '13',
     enName: 'ForResCard',
-    cnName: '外国人居住证'
+    cnName: '外国人居住证',
+    available: false
   },
   Passport: {
     code: '99',
     enName: 'Passport',
-    cnName: '护照'
+    cnName: '护照',
+    available: false
   },
   
   // 获取所有证件类型列表
