6655cfd4c426ee206d1b32f7a51a1da22b07bdf6
Asial Jim <asialjim@qq.com>
2025-10-19 01:47:55 +0000

    实现rest和admin函数，抽取公共代码为buildUrl函数

--- utils/url.js
+++ utils/url.js
@@ -2,19 +2,20 @@
 const { baseUrl, apiPrefix } = require('./env');
 
 /**
- * 构建开放页面URL
- * @param {string} uri - 要打开的页面路径(必须以 / 开头，如果不以 / 开头则在功能中添加 / 前缀)
- * @param {Object} queries - 要传递的查询参数对象，格式为 {key: value}
+ * 构建基础URL的通用函数
+ * @param {string} type - URL类型（open/rest/admin）
+ * @param {string} uri - 要打开的页面路径
+ * @param {Object} queries - 查询参数对象
  * @returns {string} 完整的URL
  */
-function open(uri, queries = {}) {
+function buildUrl(type, uri, queries = {}) {
   // 检查并处理uri前缀
   if (!uri.startsWith('/')) {
     uri = '/' + uri;
   }
   
   // 构建基础URL
-  let url = baseUrl + apiPrefix + '/open' + uri;
+  let url = baseUrl + apiPrefix + '/' + type + uri;
   
   // 处理查询参数
   if (queries && typeof queries === 'object' && Object.keys(queries).length > 0) {
@@ -34,5 +35,35 @@ function open(uri, queries = {}) {
   return url;
 }
 
-// 导出open函数
-module.exports = { open };
+/**
+ * 构建开放页面URL
+ * @param {string} uri - 要打开的页面路径(必须以 / 开头，如果不以 / 开头则在功能中添加 / 前缀)
+ * @param {Object} queries - 要传递的查询参数对象，格式为 {key: value}
+ * @returns {string} 完整的URL
+ */
+function open(uri, queries = {}) {
+  return buildUrl('open', uri, queries);
+}
+
+/**
+ * 构建REST API URL
+ * @param {string} uri - 要打开的页面路径(必须以 / 开头，如果不以 / 开头则在功能中添加 / 前缀)
+ * @param {Object} queries - 要传递的查询参数对象，格式为 {key: value}
+ * @returns {string} 完整的URL
+ */
+function rest(uri, queries = {}) {
+  return buildUrl('rest', uri, queries);
+}
+
+/**
+ * 构建管理后台URL
+ * @param {string} uri - 要打开的页面路径(必须以 / 开头，如果不以 / 开头则在功能中添加 / 前缀)
+ * @param {Object} queries - 要传递的查询参数对象，格式为 {key: value}
+ * @returns {string} 完整的URL
+ */
+function admin(uri, queries = {}) {
+  return buildUrl('admin', uri, queries);
+}
+
+// 导出所有函数
+module.exports = { open, rest, admin };
